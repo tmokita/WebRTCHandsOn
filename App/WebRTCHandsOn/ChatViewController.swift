9437d66a1f2dfce2e71e1c5ee495941a24825cf2
tmokita <tmokita@gmail.com>
2017-06-18 14:08:04 +0900

    wip

--- App/WebRTCHandsOn/ChatViewController.swift
+++ App/WebRTCHandsOn/ChatViewController.swift
@@ -20,15 +20,19 @@ class ChatViewController: UIViewController, WebSocketDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // WebSocket Initialize
+        let url  = URL(string: "wss://conf.space/WebRTCHandsOnSig/tmokita")!
+        webSocket = WebSocket(url: url)
+        webSocket.delegate = self
+        webSocket.connect()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func onClose(_ sender: Any) {
+        webSocket.disconnect()
         self.navigationController?.popViewController(animated: true)
     }
     
@@ -37,17 +41,8 @@ class ChatViewController: UIViewController, WebSocketDelegate {
     
     @IBAction func onHangUp(_ sender: Any) {
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
-    // MARK: - LOG func
+    // MARK: - LOG
     func LOG(_ body: String = "",
              function: String = #function,
              line: Int = #line)
